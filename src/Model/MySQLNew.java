b44f15d3ca9a0f8a9b916d061006a557480a2e52
Chathusha Mendis <86899589+mendisofficial@users.noreply.github.com>
2024-07-12 12:03:54 +0530

    Added new connection class

---
+++ src/Model/MySQLNew.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author chathushamendis
+ */
+public class MySQLNew {
+
+    private static Connection connection = null;
+
+    public static void createConnection() throws Exception {
+        if (connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sad-test", "root", "root");
+        }
+
+    }
+
+    public static ResultSet executeSearch(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeQuery(query);
+    }
+
+    public static Integer executeIUD(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeUpdate(query);
+    }
+}
