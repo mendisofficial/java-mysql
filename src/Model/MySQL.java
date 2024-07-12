4fecbb9ea96715d987251056ad239bae19038f49
Chathusha Mendis <86899589+mendisofficial@users.noreply.github.com>
2024-07-12 12:04:18 +0530

    optimized the first connection class

--- src/Model/MySQL.java
+++ src/Model/MySQL.java
@@ -6,33 +6,29 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 public class MySQL {
+
     private static Connection connection = null;
-    
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sad-test", "root", "root");
-            
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sad-test", "root", "root");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public static ResultSet execute(String query) {
-        try {  
-            Statement statement = connection.createStatement();
-            
-            if (query.startsWith("SELECT")) {
-                ResultSet resultset = statement.executeQuery(query);
-                return resultset;
-            } else {
-                int result = statement.executeUpdate(query);
-                return null;
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+
+    public static ResultSet execute(String query) throws Exception {
+        Statement statement = connection.createStatement();
+
+        if (query.startsWith("SELECT")) {
+            ResultSet resultset = statement.executeQuery(query);
+            return resultset;
+        } else {
+            int result = statement.executeUpdate(query);
             return null;
         }
+
     }
 }
